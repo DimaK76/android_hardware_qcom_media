3257efd907df4af17c0bb19f6cc51ffbc519dd7c
Flour_Mo <1561707299@qq.com>
2015-12-14 17:45:01 +0800

    Update support for 3s

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -4427,57 +4427,6 @@ bool venc_dev::venc_set_vpx_error_resilience(OMX_BOOL enable)
     return true;
 }
 
-bool venc_dev::venc_set_session_priority(OMX_U32 priority) {
-    struct v4l2_control control;
-
-    control.id = V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY;
-    switch(priority) {
-        case 0:
-            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_ENABLE;
-            break;
-        case 1:
-            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE;
-            break;
-        default:
-            priority = 1;
-            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE;
-            DEBUG_PRINT_ERROR("Unsupported priority level %u", priority);
-            break;
-    }
-
-    if (ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control)) {
-        DEBUG_PRINT_ERROR("Failed to set V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_%s",
-                priority == 0 ? "ENABLE" : "DISABLE");
-        return false;
-    }
-
-    sess_priority.priority = priority;
-
-    DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, val=%d",
-            control.id, control.value);
-    return true;
-}
-
-bool venc_dev::venc_set_operatingrate(OMX_U32 rate) {
-    struct v4l2_control control;
-
-    control.id = V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE;
-    control.value = rate;
-
-    DEBUG_PRINT_LOW("venc_set_operating_rate: %d fps", rate >> 16);
-    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
-
-    if(ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control)) {
-        hw_overload = errno == EBUSY;
-        DEBUG_PRINT_ERROR("Failed to set operating rate %d fps (%s)",
-                rate >> 16, hw_overload ? "HW overload" : strerror(errno));
-        return false;
-    }
-    operating_rate = rate;
-    DEBUG_PRINT_LOW("Operating Rate Set = %d fps",  rate >> 16);
-    return true;
-}
-
 bool venc_dev::venc_get_profile_level(OMX_U32 *eProfile,OMX_U32 *eLevel)
 {
     bool status = true;
