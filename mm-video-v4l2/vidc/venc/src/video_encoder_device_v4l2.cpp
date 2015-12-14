158b1010e404f546e7d3900d90d2800e08589475
Flour_Mo <1561707299@qq.com>
2015-12-14 20:53:02 +0800

    venc:delete venc_set_session_priority

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -253,7 +253,6 @@ venc_dev::venc_dev(class omx_venc *venc_class)
     enable_mv_narrow_searchrange = false;
     supported_rc_modes = RC_ALL;
     memset(&ltrinfo, 0, sizeof(ltrinfo));
-    sess_priority.priority = 1;
     operating_rate = 0;
 
     char property_value[PROPERTY_VALUE_MAX] = {0};
@@ -1052,11 +1051,6 @@ bool venc_dev::venc_open(OMX_U32 codec)
         }
     }
 
-    sess_priority.priority = 1; /* default to non-real-time */
-    if (venc_set_session_priority(sess_priority.priority)) {
-        DEBUG_PRINT_ERROR("Setting session priority failed");
-        return OMX_ErrorUnsupportedSetting;
-    }
     return true;
 }
 
@@ -2088,26 +2082,6 @@ bool venc_dev::venc_set_config(void *configData, OMX_INDEXTYPE index)
                 }
                 break;
             }
-        case OMX_IndexConfigPriority:
-            {
-                OMX_PARAM_U32TYPE *priority = (OMX_PARAM_U32TYPE *)configData;
-                DEBUG_PRINT_LOW("Set_config: priority %u",priority->nU32);
-                if (!venc_set_session_priority(priority->nU32)) {
-                    DEBUG_PRINT_ERROR("Failed to set priority");
-                    return false;
-                }
-                break;
-            }
-        case OMX_IndexConfigOperatingRate:
-            {
-                OMX_PARAM_U32TYPE *rate = (OMX_PARAM_U32TYPE *)configData;
-                DEBUG_PRINT_LOW("Set_config: operating rate %d", rate->nU32);
-                if (!venc_set_operatingrate(rate->nU32)) {
-                    DEBUG_PRINT_ERROR("Failed to set operating rate");
-                    return false;
-                }
-                break;
-            }
         default:
             DEBUG_PRINT_ERROR("Unsupported config index = %u", index);
             break;
@@ -2334,8 +2308,6 @@ void venc_dev::venc_config_print()
     DEBUG_PRINT_HIGH("ENC_CONFIG: VUI timing info enabled: %d", vui_timing_info.enabled);
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: Peak bitrate: %d", peak_bitrate.peakbitrate);
-
-    DEBUG_PRINT_HIGH("ENC_CONFIG: Session Priority: %u", sess_priority.priority);
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: Operating Rate: %u", operating_rate);
 }
